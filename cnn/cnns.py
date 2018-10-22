c365f8863259ee13aab56ee54a98f8baf7381abf
Roma Ahuja <roma.s.ahuja@gmail.com>
2018-10-21 17:37:17 -0700

    adding basic model for cnns

---
+++ cnn/cnns.py
@@ -0,0 +1,14 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=(2,2), strides=1, padding='same', activation='relu', input_shape=(32,32,3)))
+model.add(MaxPooling2D(pool_size=(2,2), strides=2, padding='valid'))
+model.add(Conv2D(filters=32, kernel_size=(2,2), strides=1, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=2, padding='valid'))
+model.add(Conv2D(filters=64, kernel_size=(2,2), strides=1, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=2, padding='valid'))
+model.add(Flatten())
+model.add(Dense(512), activation='relu')
+model.add(Dense(10), activation='softmax')
+model.summary()
